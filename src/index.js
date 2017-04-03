99e0a01cb468f898677b9a3c25b5d5cd612b8fa7
Guilherme Nogueira <guilhermenogueira@univicosa.com.br>
2017-04-03 15:29:50 -0300

    Changing index.js

--- src/index.js
+++ src/index.js
@@ -6,21 +6,36 @@ import routes from './routes';
 import RootContainer from './app/containers/RootContainer';
 import 'bootstrap/dist/css/bootstrap.css';
 
-const renderApp = (routes) => {
-  render(
-    <AppContainer>
-      <RootContainer routes={routes} />
-    </AppContainer>,
-    document.querySelector('#root')
-  );
+const __DEV__ = process.env.NODE_ENV === 'development';
+
+if (__DEV__) {
+  const renderApp = (routes) => {
+    render(
+      <AppContainer>
+        <RootContainer routes={routes} />
+      </AppContainer>,
+      document.querySelector('#root')
+    );
+  }
+
+  renderApp(routes);
+
+  if (module.hot) {
+    module.hot.accept('./routes', () => {
+      const newRoutes = require('./routes').default;
+
+      renderApp(newRoutes);
+    });
+  }
 }
 
-renderApp(routes);
+if (! __DEV__) {
+  const renderApp = (routes) => {
+    render(
+      <RootContainer routes={routes} />,
+      document.querySelector('#root')
+    );
+  }
 
-if (module.hot) {
-  module.hot.accept('./routes', () => {
-    const newRoutes = require('./routes').default;
-
-    renderApp(newRoutes);
-  });
+  renderApp(routes);
 }
